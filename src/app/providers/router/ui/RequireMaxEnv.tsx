183ac2fe3a6cb1745bda4dfaac52efeff7c8a8d1
Melevate <32_melevate@mail.ru>
2025-11-11 16:50:02 +0300

    update RequireMaxEnv and fix package.json

--- src/app/providers/router/ui/RequireMaxEnv.tsx
+++ src/app/providers/router/ui/RequireMaxEnv.tsx
@@ -1,5 +1,8 @@
+import { Button, Typography } from "@maxhub/max-ui";
 import type { JSX } from "react";
+import { classNames } from "@/shared/lib/classNames/classNames.ts";
 import { useMax } from "@/shared/lib/hooks/useMax.ts";
+import cls from "./RequireMaxEnv.module.scss";
 
 export function RequireMaxEnv({ children }: { children?: JSX.Element }) {
 	const { isInMax } = useMax();
@@ -9,7 +12,27 @@ export function RequireMaxEnv({ children }: { children?: JSX.Element }) {
 	}
 
 	if (!isInMax) {
-		return <div>Откройте данное приложение в MAX</div>;
+		return (
+			<div className={classNames(cls.requireMaxEnv, {}, [])}>
+				<Typography.Action variant="large">
+					Откройте данное приложение в Max
+				</Typography.Action>
+				<Button
+					appearance="themed"
+					asChild
+					mode="primary"
+					size="medium"
+				>
+					<a
+						href="https://web.max.ru/11929912"
+						rel="noreferrer"
+						target="_blank"
+					>
+						Перейти в Max!
+					</a>
+				</Button>
+			</div>
+		);
 	}
 
 	return <>{children}</>;
