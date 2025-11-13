f00f552ad0c6e046a085b6b85e582dec87015c26
Melevate <32_melevate@mail.ru>
2025-11-13 17:00:16 +0300

    fix fetchCourtsCord api

--- src/entities/CourtCord/model/services/fetchCourtsCord.ts
+++ src/entities/CourtCord/model/services/fetchCourtsCord.ts
@@ -3,28 +3,28 @@ import type { ThunkConfig } from "@/app/providers/StoreProvider";
 import type { CourtType } from "@/entities/Court";
 import type { CourtsCords } from "../types/courtsCords.ts";
 
-const MOCK_COURTS: CourtsCords[] = [
-	{
-		id: "123e4567-e89b-12d3-a456-426614174000",
-		lat: 47.23683,
-		lon: 39.712619,
-	},
-	{
-		id: "3",
-		lat: 47.224444,
-		lon: 39.705,
-	},
-	{
-		id: "4",
-		lat: 47.221136,
-		lon: 39.70797,
-	},
-	{
-		id: "5",
-		lat: 47.23,
-		lon: 39.7158,
-	},
-];
+// const MOCK_COURTS: CourtsCords[] = [
+// 	{
+// 		id: "123e4567-e89b-12d3-a456-426614174000",
+// 		lat: 47.23683,
+// 		lon: 39.712619,
+// 	},
+// 	{
+// 		id: "3",
+// 		lat: 47.224444,
+// 		lon: 39.705,
+// 	},
+// 	{
+// 		id: "4",
+// 		lat: 47.221136,
+// 		lon: 39.70797,
+// 	},
+// 	{
+// 		id: "5",
+// 		lat: 47.23,
+// 		lon: 39.7158,
+// 	},
+// ];
 
 interface FetchCourtsCordParams {
 	cityId: string;
@@ -39,21 +39,21 @@ export const fetchCourtsCord = createAsyncThunk<
 	const { rejectWithValue, extra } = thunkApi;
 
 	try {
-		// const response = await extra.api.get<{ message: string; courtsCord: CourtsCords[] }>("/api/courts-cord", {
-		//   params: {
-		//     cityId: params.cityId,
-		//     sport: params.sport,
-		//   },
-		// });
+		const response = await extra.api.get<{ message: string; courtsCord: CourtsCords[] }>("/courts-service/v1/courts/search/points", {
+		  params: {
+		    cityId: params.cityId,
+		    sport: params.courtType,
+		  },
+		});
 
-		// if (!response.data || !response.data.courtsCord) {
-		//   throw new Error("No data received");
-		// }
+		if (!response.data || !response.data.courtsCord) {
+		  throw new Error("No data received");
+		}
 
-		// return response.data.courtsCord;
+		return response.data.courtsCord;
 
 		// Пока мок:
-		return MOCK_COURTS;
+		// return MOCK_COURTS;
 	} catch (e) {
 		return rejectWithValue("Не удалось получить данные о площадках");
 	}
